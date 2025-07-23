7b9fe9dbae9416a3a3fecb5716055e3ec77cdfd3
Dholakia Vaibhav <vdholakia04@gmail.com>
2025-07-23 16:18:13 +0530

    Boiler plate and fonts setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { Box } from "@mui/material";
 import "./App.css";
+import { Route, Routes } from "react-router";
+import { Home } from "./modules";
 
 const App = () => {
-  return <div>HELLLO</div>;
+  return (
+    <Box>
+      <Routes>
+        <Route index element={<Home />} />
+      </Routes>
+    </Box>
+  );
 };
 
 export default App;
---
+++ src/modules/Home/Home.tsx
@@ -0,0 +1,7 @@
+import { Box } from "@mui/material";
+
+const Home = () => {
+  return <Box className="text-3xl font-bold">HOME</Box>;
+};
+
+export default Home;
