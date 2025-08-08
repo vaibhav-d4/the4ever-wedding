b0d45f8c0458ff2a56083355038f34f0a5455619
Dholakia Vaibhav <vdholakia04@gmail.com>
2025-08-08 20:40:06 +0530

    Update Grid spacing in Events component for improved layout

--- src/modules/Events/Events.tsx
+++ src/modules/Events/Events.tsx
@@ -9,7 +9,7 @@ const Events = () => {
       <Box className="text-center text-2xl font-dancing-script my-10">
         Join us in celebrating these beautiful moments of love, tradition, and togetherness.
       </Box>
-      <Grid container spacing={2} className="">
+      <Grid container spacing={6} className="">
         {WEDDING_EVENTS.map((event) => (
           <Grid size={{xs: 12, sm: 6, md: 4}} key={event.name} sx={{display: "flex", justifyContent: "center"}}>
             <EventCard {...event} />
