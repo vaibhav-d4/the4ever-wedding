05bb9c6cd7fc65a4a0014f4818b7e5f2d0c5bc4c
Dholakia Vaibhav <51409319+vaibhav-d4@users.noreply.github.com>
2025-10-26 04:18:40 +0000

    preload fixed

--- src/main.tsx
+++ src/main.tsx
@@ -16,12 +16,12 @@ if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
       .register('/service-worker.js')
-      // .then((registration) => {
-      //   console.log('ServiceWorker registration successful');
-      // })
-      // .catch((err) => {
-      //   console.log('ServiceWorker registration failed: ', err);
-      // });
+      .then(() => {
+        console.log('ServiceWorker registration successful');
+      })
+      .catch((err) => {
+        console.log('ServiceWorker registration failed: ', err);
+      });
   });
 }
 
